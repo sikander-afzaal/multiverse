71249c715ef621cfcb3dfd59529d5de3a9860c44
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-06-01 20:25:54 +0500

    first

--- src/Pages/Hero/Hero.jsx
+++ src/Pages/Hero/Hero.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import "./Hero.css";
 import { Fade } from "react-awesome-reveal";
+
+import "./Hero.css";
+import vid from "../../Assets/hero-video.mp4";
+import logo from "../../Assets/logo.svg";
 function Hero() {
   return (
     <div className={"hero"}>
+      <video src={vid} controls={false} loop autoPlay muted />
       <Fade triggerOnce direction="down" duration={2000}>
         <div className="container-wrapper-hero">
-          <h1 className="">MULTIVERSE STUDIO</h1>
+          <img src={logo} alt="" className="logo" />
           <p>Bringing Utility to Web 3</p>
         </div>
       </Fade>
